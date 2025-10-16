b85dcaf282401ceab23791980fc1e091cf4aab44
shahad <shada-123@hotmail.com>
2025-10-16 11:02:55 +0300

    Initial Commit

---
+++ plants app/plants_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  plants_appApp.swift
+//  plants app
+//
+//  Created by shahad khaled on 24/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct plants_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
